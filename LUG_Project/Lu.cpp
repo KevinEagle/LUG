f0d596f0cae909401e15a570155418bbdc94bdb2
Kevin Sienkiewicz <kevinheja@op.pl>
2021-05-22 23:33:00 +0200

    Fix something i Lu class

--- LUG_Project/Lu.cpp
+++ LUG_Project/Lu.cpp
@@ -9,132 +9,102 @@ Lu::Lu()
 
 Lu::~Lu() //destruktor
 {
-	for (int i = 0; i < numberOfRows; i++) {
+	for (int i = 0; i < n; i++) {
 		delete[] tab_lu[i];
 	}
 	delete[] tab_lu;
 }
-void Lu::decomposition(float a[10][10], float l[10][10], float u[10][10], int n)
+void Lu::decomposition()
 {
+	float ** l;
+	float ** u;
+
+	l = new float*[n];
+	u = new float*[n];
+	for (int i = 0; i < n; i++)
 	{
-		int i = 0, j = 0, k = 0;
-		for (i = 0; i < n; i++)
+		l[i] = new float[n];
+		u[i] = new float[n];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
 		{
-			for (j = 0; j < n; j++)
+			if (j < i)
+				l[j][i] = 0;
+			else
 			{
-				if (j < i)
-					l[j][i] = 0;
-				else
+				l[j][i] = tab_lu[j][i];
+				for (int k = 0; k < i; k++)
 				{
-					l[j][i] = a[j][i];
-					for (k = 0; k < i; k++)
-					{
-						l[j][i] = l[j][i] - l[j][k] * u[k][i];
-					}
+					l[j][i] = l[j][i] - l[j][k] * u[k][i];
 				}
 			}
-			for (j = 0; j < n; j++)
-			{
-				if (j < i)
-					u[i][j] = 0;
-				else if (j == i)
-					u[i][j] = 1;
-				else {
-					u[i][j] = a[i][j] / l[i][i];
-					for (k = 0; k < i; k++) {
-						u[i][j] = u[i][j] - ((l[i][k] * u[k][j]) / l[i][i]);
-					}
+		}
+		for (int j = 0; j < n; j++)
+		{
+			if (j < i)
+				u[i][j] = 0;
+			else if (j == i)
+				u[i][j] = 1;
+			else {
+				u[i][j] = tab_lu[i][j] / l[i][i];
+				for (int k = 0; k < i; k++)
+				{
+					u[i][j] = u[i][j] - ((l[i][k] * u[k][j]) / l[i][i]);
 				}
 			}
 		}
 	}
+
 	cout << "Wyglad macierzy po dekompozycji czesci L: " << endl;
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+		{
 			cout << l[i][j] << " ";
 		}
 		cout << endl;
 	}
 	cout << "Wyglad macierzy po dekompozycji czesci U: " << endl;
-	for (i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 
 
-		for (j = 0; j < n; j++) {
+		for (int j = 0; j < n; j++)
+		{
 			cout << u[i][j] << " ";
 		}
 		cout << endl;
 	}
-	return;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] l[i];
+		delete[] u[i];
+	}
+	delete[] l;
+	delete[] u;
 
 }
 
-//void Lu::lu_results() {
-	//float a[10][10], l[10][10], u[10][10];
-	//int n = 0, i = 0, j = 0;
-
-	///*decomposition(a, l, u, n);
-	//cout << "Wyglad macierzy po dekompozycji czesci L: " << endl;
-	//for (i = 0; i < n; i++) {
-	//	for (j = 0; j < n; j++) {
-	//		cout << l[i][j] << " ";
-	//	}
-	//	cout << endl;
-	//}
-	//cout << "Wyglad macierzy po dekompozycji czesci U: " << endl;
-	//for (i = 0; i < n; i++) {
-
-
-	//	for (j = 0; j < n; j++) {
-	//		cout << u[i][j] << " ";
-	//	}
-	//	cout << endl;
-	//}
-	//return;*/
-//}
-
-
-//void Lu::lu_input()
-//{
-//
-//	int * tab_1 = new int*[i];
-//	//int n = 0, i = 0, j = 0;
-//
-//	cout << "Prosze wpisac wielkosc macierzy : " << endl;
-//	cin >> n;
-//
-//	cout << "Prosze wpisac wartosci macierzy kolumnami: " << endl;
-//	for (i = 0; i < n; i++)
-//	{
-//		for (j = 0; j < n; j++)
-//		{
-//			cin >> a[i][j];
-//		}
-//
-//	}
-//
-//
-//	return;
-//}
-
 void Lu::initMatrix() //jest
 {
-	cout << "Wpisz ilosc wierszy" << endl;
-	cin >> numberOfRows;
-	cout << "Wpis ilosc kolumn" << endl;
-	cin >> numberOfColumns;
-	tab_lu = new float* [numberOfRows];
-	for (int i = 0; i < numberOfRows; i++) {
-		tab_lu[i] = new float[numberOfColumns];
+	cout << "Wpisz wielkosc macierzy" << endl;
+	cin >> n;
+	tab_lu = new float*[n];
+	for (int i = 0; i < n; i++) {
+		tab_lu[i] = new float[n];
 	}
 }
 
 void Lu::fillMatrix() //jest
 {
 	cout << "Wpisz wartosci macierzy: " << endl;
-	for (int i = 0; i < numberOfRows; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << "Kolumna " << i << endl;
-		for (int j = 0; j < numberOfColumns; j++)
+		for (int j = 0; j < n; j++)
 		{
 			cout << "Wiersz" << j;
 			cin >> tab_lu[i][j];
@@ -144,8 +114,8 @@ void Lu::fillMatrix() //jest
 
 void Lu::printMatrix() //jest
 {
-	for (int i = 0; i < numberOfRows; i++) {
-		for (int j = 0; j <= numberOfColumns; j++)
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
 			cout << tab_lu[i][j] << " ";
 		cout << endl;
 	}
@@ -162,19 +132,18 @@ void Lu::loadFromFile(string name)
 		cout << "Nie ma takiego pliku." << endl;
 	}
 
-	file >> numberOfRows;
-	file >> numberOfColumns;
+	file >> n;
 
-	tab_lu = new float* [numberOfRows];
-	for (int i = 0; i < numberOfRows; i++)
+	tab_lu = new float*[n];
+	for (int i = 0; i < n; i++)
 	{
-		tab_lu[i] = new float[numberOfColumns];
+		tab_lu[i] = new float[n];
 	}
 
 
-	for (int i = 0; i < numberOfRows; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < numberOfColumns; j++)
+		for (int j = 0; j < n; j++)
 		{
 			file >> tab_lu[i][j];
 
--- LUG_Project/Lu.h
+++ LUG_Project/Lu.h
@@ -10,15 +10,13 @@ using namespace std;
 class Lu
 {
 private:
-	int i, j, n;
-	int numberOfRows;
-	int numberOfColumns;
+	int n; // matrix size
 	float ** tab_lu;
 
 public:
 	Lu();
 	~Lu();
-	void decomposition(float a[10][10], float l[10][10], float u[10][10], int n);
+	void decomposition();
 	void initMatrix();
 	void fillMatrix();
 	void printMatrix();
